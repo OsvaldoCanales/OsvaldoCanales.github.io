2b995188cab141e385fbb4cdee6d6b82e8057c5c
Oz <osvaldocanales4@gmail.com>
2024-05-13 14:48:43 -0600

    Added CSS customs to homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-import logo from '../assets/img/logo.svg'
-import navIcon1 from'../assets/img/nav-icon1.svg'
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
 import navIcon3 from '../assets/img/nav-icon3.svg'
 
-
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, seScrolled] = useState(false);
+    const [scrolled, seScrolled] = useState(false); 
 
+    
     useEffect(() => {
         console.log('use Effect ran')
+        //Function returns boolean true or false if user has scrolled
         const onScroll = () => {
             if (window.scrollY > 50) {
                 seScrolled(true);
@@ -23,7 +24,7 @@ export const NavBar = () => {
             }
         }
 
-        window.addEventListener("scroll", onScroll);
+        window.addEventListener("scroll", onScroll); //Invokes the onscroll function when user scrolls
 
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
@@ -36,14 +37,14 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
                 <Navbar.Brand href = '#home'>
-                    <img src={logo} alt = "Logo" />
+                    <img src={logo} alt = "Logo1" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className = "navbar-toggler-icon"></span>
                 </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id = "basic-navbar-nav">
                     <Nav className = "ms-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'activ navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                         <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                         <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
                     </Nav>
@@ -53,7 +54,7 @@ export const NavBar = () => {
                             <a href="#"><img src={navIcon2} alt= "" /></a>
                             <a href="#"><img src={navIcon3} alt= "" /></a>
                         </div>
-                        <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+                        <button className="vvd" ><span>Let's Connect</span></button>
                     </span>
                 </Navbar.Collapse>
             </Container>
