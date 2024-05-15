ed66f10b023ed807cfe942ad42c90427b6f3dee1
Oz <osvaldocanales4@gmail.com>
2024-05-15 11:11:34 -0600

    Current Progress CSS Skill section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,7 +37,7 @@ export const NavBar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
             <Container>
                 <Navbar.Brand href = '#home'>
-                    <img src={logo} alt = "Logo1" />
+                    <img src={logo} alt = "Logo1" /> 
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className = "navbar-toggler-icon"></span>
